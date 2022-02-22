673cff4890a8a950f3f2616808d5123ecc0ade50
Salwa Elsayed <43003474+Salwa-Abosafeen@users.noreply.github.com>
2022-02-22 10:14:31 +0200

    Adding prettier and eslint to the project

--- build/routes/index.js
+++ build/routes/index.js
@@ -1,11 +1,18 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = __importDefault(require("express"));
-var imageResizeCheck_1 = require("../utils/imageResizeCheck");
-var images_1 = __importDefault(require("./api/images"));
+'use strict';
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
+Object.defineProperty(exports, '__esModule', { value: true });
+var express_1 = __importDefault(require('express'));
+var imageResizeCheck_1 = require('../utils/imageResizeCheck');
+var images_1 = __importDefault(require('./api/images'));
 var routes = express_1.default.Router();
-routes.use("/images", imageResizeCheck_1.checkIfImageFullExists, imageResizeCheck_1.checkIfImageThumbnailExists, images_1.default);
+routes.use(
+  '/images',
+  imageResizeCheck_1.checkIfImageFullExists,
+  imageResizeCheck_1.checkIfImageThumbnailExists,
+  images_1.default
+);
 exports.default = routes;
